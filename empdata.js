327c86c76a804606904938af513f104f666215cd
brand <brandj2@seattleu.edu>
2018-05-21 12:52:03 -0700

    update

--- empdata.js
+++ empdata.js
@@ -4,19 +4,19 @@
       "name": "Jesse Brand",
       "position": "Business Intelligence Analyst",
       "bonus": "$100,000",
-      "promotion_date": "2018/05/25",
+      "promotion_date": "2018/05/25"
     },
     {
       "name": "James Bond",
       "position": "Spy",
       "bonus": "$777,000",
-      "promotion_date": "2011/07/25",
+      "promotion_date": "2011/07/25"
     },
     {
       "name": "Charles Carmicheal",
       "position": "Technician",
       "bonus": "$66,000",
-      "promotion_date": "2009/01/12",
+      "promotion_date": "2009/01/12"
     }
   ]
 }
